c2184aa56fef7ec48e686708a5d9935a6599d019
Cameron Strandberg <cameron.strandberg@gmail.com>
2018-01-07 15:58:24 -0800

    Mongo, passport with google setup

---
+++ services/passports.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+const User = mongoose.model('user');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      new User({ googleId: profile.id }).save();
+    }
+  )
+);
