ecfd31eead15232e42420661a0b51d6f00d06c03
Cameron Strandberg <cameron.strandberg@gmail.com>
2018-01-12 15:47:18 -0800

    Rigged

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport')
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passports');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+
+app.use(cookieSession({
+  // 30 days in milliseconds
+  maxAge: 20 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
