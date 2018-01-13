e28a3f58e5a0677709b5f20539d6e62300f547f6
Cameron Strandberg <cameron.strandberg@gmail.com>
2018-01-12 16:33:58 -0800

    More stuff

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  cookieKey: process.env.COOKIE_ID,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI
+};
